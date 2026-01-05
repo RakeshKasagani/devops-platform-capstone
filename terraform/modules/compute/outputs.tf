2a82ebe4a03d134f4e530737b0aa2b2056404f90
RakeshKasagani <rakeshkasagani121@gmail.com>
2026-01-05 09:49:50 +0530

    Create outputs.tf

---
+++ terraform/modules/compute/outputs.tf
@@ -0,0 +1,19 @@
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+
+output "sonarqube_public_ip" {
+  value = aws_instance.sonarqube.public_ip
+}
+
+output "nexus_public_ip" {
+  value = aws_instance.nexus.public_ip
+}
+
+output "docker_public_ip" {
+  value = aws_instance.docker.public_ip
+}
+# output "ansible_public_ip" {
+#   value =  aws_instance.ansible.public_ip
+
+# }
